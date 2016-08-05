11ec06ce9742d2eee9e382ad028d8991942ef7bd
Vikram Karthikeyan <ak.vikram007@gmail.com>
2016-08-05 12:49:34 +0530

    Updated mainclass

--- MainClass.java
+++ MainClass.java
@@ -3,9 +3,9 @@ package scrabble;
 public class MainClass {
 
 	public static void main(String[] args) {
-		Character[] rackList = {'A','B'};
+		Character[] rackList = {'A','B','C','E','T','P','U'};
 		Rack a = new Rack(rackList);
-		System.out.println(a);
+		System.out.println(a.getBestScore());
 	}
 
 }
